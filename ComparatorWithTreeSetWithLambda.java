438a5ff806270cc1a54319de0fada456d74f7df6
roshanpatida <roshan.patidar@brainvire.com>
2023-08-09 14:34:56 +0530

    h

---
+++ ComparatorWithTreeSetWithLambda.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package section2lambdaexpressionwithcollection;
+
+/**
+ *
+ * @author roshan_patidar
+ */
+
+import java.util.Comparator;
+import java.util.TreeSet;
+ 
+ class ComparatorWithTreeSetWithLambda
+ {
+ public static void main(String[] args)
+ {
+ TreeSet<Integer> l = new TreeSet<Integer>((I1,I2)->(I1>I2)?-1:(I1<I2)?1:0);
+ l.add(10);
+ l.add(0);
+ l.add(15);
+ l.add(5);
+ l.add(20);
+ System.out.println(l);
+ }
+ } 
