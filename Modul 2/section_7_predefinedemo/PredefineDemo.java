cc55349512292185b5a8ff2a8a33ecd4c25bbf53
roshanpatida <roshan.patidar@brainvire.com>
2023-08-16 14:09:26 +0530

    Predefined Functional Interface - Predicate

---
+++ Modul 2/section_7_predefinedemo/PredefineDemo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package section_7_predefinedemo;
+
+
+ import java.util.function.*;
+import java.util.function.Function;
+ class PredefineDemo {
+ public static void main(String[] args) {
+ Function<String, Integer> f = s ->s.length();
+ System.out.println(f.apply("Durga"));
+ System.out.println(f.apply("Soft")); }}
